d134d548fff14b7a072a0ac3cb1162c458b8b6a4
MikeReed-13 <mikereed013@gmail.com>
2020-09-01 13:55:19 -0400

    Initial check in

---
+++ app/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.scss';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>React-Redux-Boilerplate</div>
+        )
+    }
+}
+
+export default App;
